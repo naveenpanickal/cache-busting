69ed4b66f43125c9d7f40bbf4252343561fb8e8c
naveen <naveen@neoito.com>
2022-05-09 18:34:28 +0530

    initial commit

---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.scss']
+})
+export class ContactComponent implements OnInit {
+
+  @ViewChild('refreshForm', { static: false }) refreshForm: any;
+
+  forceReload() {
+    this.refreshForm.nativeElement.submit();
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    setTimeout(()=> { console.log("HELLO");
+    window.location.href = window.location.href
+    }, 5000)
+
+  }
+
+}
